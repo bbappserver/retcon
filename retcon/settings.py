abf62e2959765075c468b2e3374a3a8083fa11e4
That guy on the discord <thatguy@null.com>
2019-12-25 14:50:42 -0800

    Set API policy to token auth for non-reading operations.

--- retcon/settings.py
+++ retcon/settings.py
@@ -74,6 +74,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'rest_framework.authtoken',
     'sharedstrings',
     'semantictags',
     'retconpeople',
@@ -164,9 +165,20 @@ STATIC_URL = '/static/'
 
 # DRF Pagination
 REST_FRAMEWORK = {
-    'PAGINATE_BY': 20,                 # Default to 20
-    'PAGINATE_BY_PARAM': 'page_size',  # Allow client to override, using `?page_size=xxx`.
-    'MAX_PAGINATE_BY': 100             # Maximum limit allowed when using `?page_size=xxx`.
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
+    'PAGE_SIZE': 100,
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+       'rest_framework.authentication.TokenAuthentication',
+       'rest_framework.authentication.SessionAuthentication',
+   )
+   
+    
+    # 'PAGINATE_BY': 20,                 # Default to 20
+    # 'PAGINATE_BY_PARAM': 'page_size',  # Allow client to override, using `?page_size=xxx`.
+    # 'MAX_PAGINATE_BY': 100             # Maximum limit allowed when using `?page_size=xxx`.
 }
 
 '''
--- retcon/urls.py
+++ retcon/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from rest_framework import routers
+from rest_framework.authtoken import views
 import retconpeople.api,sharedstrings.api,semantictags.api
 
 router = routers.DefaultRouter()
@@ -34,5 +35,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
     path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api-token-auth/', views.obtain_auth_token),
     path('admin/doc/', include('django.contrib.admindocs.urls'))
 ]
