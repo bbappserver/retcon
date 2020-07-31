5657dc637e1d5deca48ae0aebe3148b7a066cee3
That guy on the discord <thatguy@null.com>
2020-07-31 01:31:19 -0700

    Improve efficiency and errorhandeling of hashpaths

--- retconstorage/management/commands/hashpaths.py
+++ retconstorage/management/commands/hashpaths.py
@@ -57,23 +57,26 @@ def collect_result(t):
     #we should definitely have gotten both hashes and if we didn't something went wrong
     if sha256 is None or md5 is None:
         return
+    try:
+        with transaction.atomic():
+            nf = NamedFile.objects.filter(name=name)[0]
 
-    with transaction.atomic():
-        nf = NamedFile.objects.filter(name=name)[0]
+            mf = None
+            mfs = ManagedFile.objects.filter(sha256=sha256)
+            if not mfs.exists():
+                #Create new identity
+                mf = ManagedFile(sha256=sha256, md5=md5)
+                #TODO supply prefix if this command ever accepts one other than the default
+                nf.size=nf.stat().st_size
+                mf.save()
+            else:
+                mf = mfs[0]
 
-        mf = None
-        mfs = ManagedFile.objects.filter(sha256=sha256)
-        if mfs.count() < 1:
-            #Create new identity
-            mf = ManagedFile(sha256=sha256, md5=md5)
-            #TODO supply prefix if this command ever accepts one other than the default
-            nf.size=nf.stat().st_size
-            mf.save()
-        else:
-            mf = mfs[0]
-
-        nf.identity = mf
-        nf.save()
+            nf.identity = mf
+            nf.save()
+    except FileNotFoundError:
+        #shouldn't happen but don't crash
+        pass
 
 
 class SlowBar(progress.bar.Bar):
