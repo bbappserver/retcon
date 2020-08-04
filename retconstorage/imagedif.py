04f9cb22a7a2aea7798da4feb7eb01ac8869a108
That guy on the discord <thatguy@null.com>
2020-08-04 02:35:14 -0700

    Tests in preparation for visual comparison.

--- retconstorage/imagedif.py
+++ retconstorage/imagedif.py
@@ -1,74 +1,132 @@
 from PIL import Image,ImageFilter,ImageSequence
 import array
+
 SOBEL_MAT_X=(
-    (1,0,-1),
-    (+2,0,-2),
-    (1,0,-1)
-    )
+    1,0,-1,
+    2,0,-2,
+    1,0,-1)
+
 SOBEL_KERNEL_X=ImageFilter.Kernel((3,3), SOBEL_MAT_X, scale=None, offset=0)
 class ImageSequenceSource:
 
-    SEQUENCE_TYPE_STILL_IMAGE=0
-    SEQUENCE_TYPE_GIF=0
-    SEQUENCE_TYPE_VIDEO=0
-    def __init__(self,abspath,sequence_type):
+    SEQUENCE_TYPE_STILL_IMAGE=0 #A still immage is an image sequence of length 1
+    SEQUENCE_TYPE_GIF=1
+    SEQUENCE_TYPE_VIDEO=2
+    def __init__(self,abspath,sequence_type,keyframe_delta_threshold=.55,max_median_delta=.98,color_depth=256):
+        '''
+        If a frame changes more than kerframe_delta_threshold it will be considered a keyframe.
+        A common transition is to flash momentarily to white, but it is useless to consider this a keyframe.
+        With a max_median_delta if most of the frame has a sudden significant change this will not be counted as a keyframe.
+        The next keyframe will happen once transitioning stops and tha algorithm detects normal change from actual content frames.
+        '''
         self.path=abspath
+        self._frames=None
         self._keyframes=None
-        im = Image.open(abspath)
+        self._sequence_type=sequence_type
+        self.keyframe_delta_threshold=keyframe_delta_threshold
+        self.color_depth=color_depth
+        self.max_median_delta=max_median_delta
+
+        if color_depth != 256:
+            raise NotImplementedError('Colorspaces other than RGB8 not yet supported')
         
         if sequence_type == self.SEQUENCE_TYPE_STILL_IMAGE:
-            self._frames=[im]
-            self._keyframes=[im]
+            self._im = Image.open(abspath)
+            self._frames=[self._im]
+            self._keyframes=[self._im]
+            self._im.close()
         elif sequence_type == self.SEQUENCE_TYPE_GIF:
-            self._keyframes=ImageSequence.Iterator(im)
-            self._frames=ImageSequence.Iterator(im)
+            self._im = Image.open(abspath)
+            self._keyframes=ImageSequence.Iterator(self._im)
+            self._frames=ImageSequence.Iterator(self._im)
+            self._im.close()
+        elif sequence_type == self.SEQUENCE_TYPE_VIDEO:
+            try:
+                import cv2
+                self._im= cv2.VideoCapture(abspath)
+            except ImportError:
+                import logging
+                logger = logging.getLogger(__name__)
+                logger.error('OpenCV must be installed to visually hash videos')
+                raise ValueError('OpenCV must be installed to visually hash videos')
+
         else:
             raise NotImplementedError()
 
+    def __del__(self):
+        if self._sequence_type == self.SEQUENCE_TYPE_VIDEO:
+            self._im.release()
+        else:
+            self._im.close()
+    
     def keyframes(self):
-        return self._keyframes
+        if self._keyframes is None and self._sequence_type == self.SEQUENCE_TYPE_VIDEO:
+            return self._filter_video_keyframes()
+        else:
+            return self._keyframes
     
     def frames(self):
+        if self._frames is None and self._sequence_type == self.SEQUENCE_TYPE_VIDEO:
+            self._frames=self._load_video()
         return self._frames
 
-    # def _load_video(self):
-    #     import cv2
-    #     vidcap = cv2.VideoCapture(self.path)
-    #     return  (x for x in vidcap.read())
+    def _load_video(self):
+        try:
+            import cv2
+        except ImportError:
+            import logging
+            logger = logging.getLogger(__name__)
+            logger.error('OpenCV must be installed to visually hash videos')
+            raise ValueError('OpenCV must be installed to visually hash videos')
+        frameCount = int(self._im.get(cv2.CAP_PROP_FRAME_COUNT))
+        fc=0
+        ret=True
+        while (fc < frameCount  and ret):
+            ret,f = self._im.read()
+            fc+=1
+            yield f
     
-    # def _filter_video_keyframes(self):
-    #     cap = cv2.VideoCapture('test.mp4')
-    #     frameCount = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-    #     frameWidth = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
-    #     frameHeight = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+    def _filter_video_keyframes(self):
+        try:
+            import numpy as np
+        except ImportError:
+                logger.error('Numpy must be installed to visually hash videos')
+                raise ValueError('Numpy must be installed to visually hash videos')
+        cap = self._im
+        frameCount = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        frameWidth = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        frameHeight = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
-    #     refPhase=True
-    #     ref = np.empty(( frameHeight, frameWidth, 3), np.dtype('uint8'))
-    #     f = np.empty(( frameHeight, frameWidth, 3), np.dtype('uint8'))
-    #     reft=np.empty(( 9, 9, 3), np.dtype('uint8'))
-    #     ft=np.empty(( 9, 9, 3), np.dtype('uint8'))
+        refPhase=True
 
-    #     fc = 0
-    #     ret = True
+        #TODO This assumes RGB8
+        ref = np.empty(( frameHeight, frameWidth, 3), np.dtype('uint8'))
+        f = np.empty(( frameHeight, frameWidth, 3), np.dtype('uint8'))
+        reft=np.empty(( 9, 9, 3), np.dtype('uint8'))
+        ft=np.empty(( 9, 9, 3), np.dtype('uint8'))
 
-    #     threshF=.55
+        fc = 0
+        ret = True
 
-    #     thresh= threshF* 255 # assume 8bit color space
+        thresh_percent=self.keyframe_delta_threshold
+        max_median_delta = round(self.max_median_delta * (self.color_depth -1))
 
-    #     while (fc < frameCount  and ret):
-    #         ret,f = cap.read()
-    #         ft = cv2.resize(f,(9,9), interpolation = cv2.INTER_CUBIC)
-    #         d=ft-reft
-    #         ad=np.abs(d)
-    #         mse = np.mean(ad)
-    #         stdv=np.median(ad)
+        thresh= thresh_percent* (self.color_depth-1)#color integers are zero indexed(black)
 
-    #         if mse>thresh and stdv<251:
-    #             yield f
+        while (fc < frameCount  and ret):
+            ret,f = cap.read()
+            ft = cv2.resize(f,(9,9), interpolation = cv2.INTER_CUBIC)
+            delta=ft-reft
+            abs_delta=np.abs(delta)
+            mse = np.mean(abs_delta)
+            med=np.median(abs_delta)
 
-    #         ref=f
-    #         reft=ft
-    #         fc += 1
+            if mse>thresh and med<max_median_delta:
+                yield f
+
+            ref=f
+            reft=ft
+            fc += 1
 
         
     
@@ -99,14 +157,17 @@ def dhash(image_sequence):
         sigs.append(sig)
     return sigs
 
-class ImageComparer:
-    pass
 
 class ImageSequenceComparer:
 
+    def __init__(self,sequence_a,sequence_b):
+        self._a=sequence_a
+        self._b=sequence_b
+
     def cmp(self):
         raise NotImplementedError()
-
+        a=self._a
+        b=self._b
         #TODO b should definitely be the shorter one if possible
         i=0
         match_spans=[]
--- retconstorage/tests.py
+++ retconstorage/tests.py
@@ -1,3 +1,124 @@
 from django.test import TestCase
-
+from retconstorage.imagedif import ImageSequenceComparer,ImageSequenceSource,dhash
+from glob import glob
+import os.path
 # Create your tests here.
+
+class ImageSequenceCompareTest(TestCase):
+
+    basedir=os.path.dirname(__file__)
+
+    def testLoadStilImage(self):
+        path=os.path.join(self.basedir, 'test/big*.jpg')
+        l=glob(path)
+        if len(l)==0:
+            raise NotImplementedError("test data not found jpg")
+        for p in l:
+            with self.subTest("load "+p):
+                seq=ImageSequenceSource(p,ImageSequenceSource.SEQUENCE_TYPE_STILL_IMAGE)
+                self.assertTrue(len(list(seq.frames()))>0)
+
+    def testLoadAnimatedGIF(self):
+        path=os.path.join(self.basedir, 'test/big*.gif')
+        l=glob(path)
+        if len(l)==0:
+            raise NotImplementedError("test data not found GIF")
+        for p in l:
+            with self.subTest("load "+p):
+                seq=ImageSequenceSource(p,ImageSequenceSource.SEQUENCE_TYPE_GIF)
+                self.assertTrue(len(list(seq.frames()))>0)
+
+    def testLoadVideo(self):
+        path=os.path.join(self.basedir, 'test/big*.mp4')
+        l=glob(path)
+        if len(l)==0:
+            raise NotImplementedError("test data not found mp4")
+        for p in l:
+            with self.subTest("load "+p):
+                seq=ImageSequenceSource(p,ImageSequenceSource.SEQUENCE_TYPE_VIDEO)
+                self.assertTrue(len(list(seq.frames()))>0)
+        
+        path=os.path.join(self.basedir, 'test/big*.avi')
+        l=glob(path)
+        if len(l)==0:
+            raise NotImplementedError("test data not found avi")
+        for p in l:
+            with self.subTest("load "+p):
+                seq=ImageSequenceSource(p,ImageSequenceSource.SEQUENCE_TYPE_VIDEO)
+                self.assertTrue(len(list(seq.frames()))>0)
+
+        path=os.path.join(self.basedir, 'test/big*.wmv')
+        l=glob(path)
+        if len(l)==0:
+            raise NotImplementedError("test data not found wmv")
+        for p in l:
+            with self.subTest("load "+p):
+                seq=ImageSequenceSource(p,ImageSequenceSource.SEQUENCE_TYPE_VIDEO)
+                self.assertTrue(len(list(seq.frames()))>0)
+
+    def testCompareImageToImage(self):
+        raise NotImplementedError(self)
+
+    def testCompareVideoToShrink(self):
+        raise NotImplementedError()
+
+    def testCompareVideoToShrinkClipped(self):
+        raise NotImplementedError()
+
+    def testCompareVideoShrinkToShrinkClipped(self):
+        raise NotImplementedError()
+
+    def testCompareVideoToAnimatedGif(self):
+        raise NotImplementedError()
+
+    def testCompareVideoToStill(self):
+        raise NotImplementedError()
+        #test stills against normal
+        #test stills against shrink
+        #test stills against shrinkcut
+    
+    def testCompareStillToStill(self):
+        l=[1,2,3,4,5,6,7]
+        pairs=itertools.combinations(l)
+
+        with self.subTest('identical'):
+            raise NotImplementedError()
+            #check image against identical image
+        with self.subTest('small'):
+            raise NotImplementedError()
+            #check image agains shrunk image
+        with self.subTest('small-variant'):
+            raise NotImplementedError()
+            #check image agains variant image
+        raise NotImplementedError()
+
+class DhashTest(TestCase):
+
+    def testDhashStill(self):
+        raise NotImplementedError
+    
+    def testDhashAnimatedGIF(self):
+        raise NotImplementedError
+    
+    def testDhashVideo(self):
+        raise NotImplementedError
+    
+    def testDhashcompareStillAndStill(self):
+        raise NotImplementedError
+    def testDhashcompareStillAndVideo(self):
+        raise NotImplementedError
+    def testDhashcompareStillAndGIF(self):
+        raise NotImplementedError
+
+    def testDhashcompareGIFAndGIF(self):
+        raise NotImplementedError
+
+    def testDhashcompareGIFAndVideo(self):
+        raise NotImplementedError
+
+    def testDhashcompareVideoAndVideo(self):
+        raise NotImplementedError
+
+    def testDhashcompareAll(self):
+        '''Check that if all media types are in a table they are detected as overlapping'''
+        raise NotImplementedError
